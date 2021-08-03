a456b9a0029643112cbcd2236d74ae2477fb7278
Nathaniel Palme <robotnrp@gmail.com>
2021-08-03 18:37:41 -0500

    Extra last-minute bit for Caden

---
+++ TeamCode/src/main/java/org/timecrafters/javaClass/aubrey/CadenFirstState.java
@@ -0,0 +1,22 @@
+package org.timecrafters.javaClass.aubrey;
+
+import org.cyberarm.engine.V2.CyberarmState;
+import org.timecrafters.javaClass.samples.SampleRobot;
+
+public class CadenFirstState extends CyberarmState {
+
+    //here, you'll find some of your variables. you can add more as you need them.
+    private SampleRobot robot;
+
+    //This is the constructor. It lets other code bits run use the code you put here
+    public CadenFirstState(SampleRobot robot) {
+        this.robot = robot;
+    }
+
+    //This is a method. methods are bits of code that can be run elsewhere. 
+    //This one is set up to repeat every few milliseconds
+    @Override
+    public void exec() {
+        
+    }
+}
