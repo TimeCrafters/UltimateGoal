f84b3556add57f767f2c83d411b984223ec7dac2
scottbadger777 <37743855+scottbadger777@users.noreply.github.com>
2021-08-03 20:18:52 -0500

    Aubrey

--- TeamCode/src/main/java/org/timecrafters/javaClass/aubrey/AubreyFirstState.java
+++ TeamCode/src/main/java/org/timecrafters/javaClass/aubrey/AubreyFirstState.java
@@ -7,6 +7,9 @@ public class AubreyFirstState extends CyberarmState {
 
     //here, you'll find some of your variables. you can add more as you need them.
     private SampleRobot robot;
+    private double UwU;
+    private double OwO;
+
 
     //This is the constructor. It lets other code bits run use the code you put here
     public AubreyFirstState(SampleRobot robot) {
@@ -17,6 +20,20 @@ public class AubreyFirstState extends CyberarmState {
     //This one is set up to repeat every few milliseconds
     @Override
     public void exec() {
-        
+        UwU= engine.gamepad1.left_stick_y ;
+        OwO= engine.gamepad1. right_stick_y;
+        robot.driveBackLeft.setPower(UwU);
+
+        robot.driveFrontLeft.setPower(UwU);
+        robot.driveBackRight. setPower(OwO);
+        robot.driveBackLeft. setPower(OwO);
+
+    }
+
+    @Override
+    public void telemetry() {
+        engine. telemetry.addData("uMu",UwU);
+        engine. telemetry. addData("OmO", OwO);
+
     }
 }
