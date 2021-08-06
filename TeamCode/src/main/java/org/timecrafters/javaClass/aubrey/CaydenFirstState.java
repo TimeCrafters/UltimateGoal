8c87c1b2b566abd812f1766e4944a95381c52e8a
SafePencil <cje_2k5@aol.com>
2021-08-05 19:12:16 -0500

    a thing

--- TeamCode/src/main/java/org/timecrafters/javaClass/aubrey/CaydenFirstState.java
+++ TeamCode/src/main/java/org/timecrafters/javaClass/aubrey/CaydenFirstState.java
@@ -3,13 +3,13 @@ package org.timecrafters.javaClass.aubrey;
 import org.cyberarm.engine.V2.CyberarmState;
 import org.timecrafters.javaClass.samples.SampleRobot;
 
-public class CadenFirstState extends CyberarmState {
+public class CaydenFirstState extends CyberarmState {
 
     //here, you'll find some of your variables. you can add more as you need them.
     private SampleRobot robot;
 
     //This is the constructor. It lets other code bits run use the code you put here
-    public CadenFirstState(SampleRobot robot) {
+    public CaydenFirstState(SampleRobot robot) {
         this.robot = robot;
     }
 
@@ -17,6 +17,13 @@ public class CadenFirstState extends CyberarmState {
     //This one is set up to repeat every few milliseconds
     @Override
     public void exec() {
-        
+        robot.launchMotor.setPower(engine.gamepad1.left_stick_y);
+        robot.ringBeltMotor.setPower(engine.gamepad1.right_trigger
+        );
+        if (robot.limitSwitch.isPressed()) {
+            robot.launchMotor.setPower(0.5);
+        }else{
+            robot.launchMotor.setPower(0);
+        }
     }
 }
--- TeamCode/src/main/java/org/timecrafters/javaClass/samples/CaydenFirstEngine.java
+++ TeamCode/src/main/java/org/timecrafters/javaClass/samples/CaydenFirstEngine.java
@@ -4,10 +4,11 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.cyberarm.engine.V2.CyberarmEngine;
 import org.timecrafters.UltimateGoal.Competition.Robot;
+import org.timecrafters.javaClass.aubrey.CaydenFirstState;
 import org.timecrafters.javaClass.spencer.SpencerFirstState;
 
-@Autonomous (name = "Caden: First Program", group = "caden")
-public class CadenrFirstEngine extends CyberarmEngine {
+@Autonomous (name = "Cayden: First Program", group = "caden")
+public class CaydenFirstEngine extends CyberarmEngine {
     
     SampleRobot robot;
 
@@ -21,6 +22,6 @@ public class CadenrFirstEngine extends CyberarmEngine {
     
     @Override
     public void setup() {
-        addState(new SpencerFirstState(robot));
+        addState(new CaydenFirstState(robot));
     }
 }
