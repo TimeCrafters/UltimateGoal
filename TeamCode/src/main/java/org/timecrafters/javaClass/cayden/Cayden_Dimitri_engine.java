75d85e190f6152fec277da134a3ca683efaa0552
SafePencil <cje_2k5@aol.com>
2021-08-19 19:20:14 -0500

    a thing

--- TeamCode/src/main/java/org/timecrafters/javaClass/cayden/Cayden_Dimitri_engine.java
+++ TeamCode/src/main/java/org/timecrafters/javaClass/cayden/Cayden_Dimitri_engine.java
@@ -4,16 +4,16 @@ import org.cyberarm.engine.V2.CyberarmEngine;
 
 public class Cayden_Dimitri_engine extends CyberarmEngine {
 
-  private Cayden_Dimitri Cayden_dimitri;
+  private Cayden_Dimitri cayden_dimitri;
 
     @Override
     public void init() {
-        Cayden_dimitri = new Cayden_Dimitri(hardwareMap);
+        cayden_dimitri = new Cayden_Dimitri(hardwareMap);
         super.init();
     }
 
     @Override
     public void setup() {
-addState();
+addState(new Cayden_Dimitri_Move(cayden_dimitri));
     }
 }
